41e0bda718eea900ad66001cc6482d156e0f0b50
Juno Joseph <87766026+Juno-Joseph@users.noreply.github.com>
2025-06-21 16:15:09 -0400

    accessing json with js file

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
-// import questions from "./questions.json" assert { type: "json"}
+fetch('./questions.json')
+    .then(response => response.json())
+    .then(data => {
+        const parsedData = data;
 
-var parsedData = JSON.parse(questions)
+        const quizContainer = document.querySelector(".quiz-container");
+        const question = document.querySelector(".quiz-container .question");
+        const options = document.querySelector(".quiz-container .options");
+        const nextBtn = document.querySelector(".quiz-container .next-btn")
+        const quizResult = document.querySelector(".quiz-result");
 
-const quizContainer = document.querySelector(".quiz-container");
-const question = document.querySelector(".quiz-container .question");
-const options = document.querySelector(".quiz-container .options");
-const nextBtn = document.querySelector(".quiz-container .next-btn")
-const quizResult = document.querySelector(".quiz-result");
+        let questionNumber = 0;
 
-let questionNumber = 0;
+        const createQuestion = () => {
+        question.innerHTML = parsedData[questionNumber].question;
+          // Populate options too if needed:
+          const optionButtons = options.querySelectorAll('.option');
+          optionButtons[0].innerText = parsedData[questionNumber].A;
+          optionButtons[1].innerText = parsedData[questionNumber].B;
+          optionButtons[2].innerText = parsedData[questionNumber].C;
+          optionButtons[3].innerText = parsedData[questionNumber].D;
+        };
 
-const createQuestion = () => {
-    question.innerHTML = parsedData[questionNumber].question;
-}
+        createQuestion();
+    })
+
+    .catch(error => console.error('Error loading JSON:', error));
+
