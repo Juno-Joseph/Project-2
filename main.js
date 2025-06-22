722d516cb9eefac57c43f02ff955d9a967b354a9
Juno Joseph <87766026+Juno-Joseph@users.noreply.github.com>
2025-06-22 00:32:56 -0400

    made js work with game html

--- main.js
+++ main.js
@@ -1,28 +1,22 @@
 fetch('./questions.json')
-    .then(response => response.json())
-    .then(data => {
-        const parsedData = data;
+  .then(response => response.json())
+  .then(data => {
+    const questions = data;
 
-        const quizContainer = document.querySelector(".quiz-container");
-        const question = document.querySelector(".quiz-container .question");
-        const options = document.querySelector(".quiz-container .options");
-        const nextBtn = document.querySelector(".quiz-container .next-btn")
-        const quizResult = document.querySelector(".quiz-result");
+    const questionElement = document.getElementById("question");
+    const choiceTexts = document.querySelectorAll(".choice-text");
 
-        let questionNumber = 0;
+    function showRandomQuestion() {
+      const randomIndex = Math.floor(Math.random() * questions.length);
+      const q = questions[randomIndex];
 
-        const createQuestion = () => {
-        question.innerHTML = parsedData[questionNumber].question;
-          // Populate options too if needed:
-          const optionButtons = options.querySelectorAll('.option');
-          optionButtons[0].innerText = parsedData[questionNumber].A;
-          optionButtons[1].innerText = parsedData[questionNumber].B;
-          optionButtons[2].innerText = parsedData[questionNumber].C;
-          optionButtons[3].innerText = parsedData[questionNumber].D;
-        };
+      questionElement.innerText = q.question;
+      choiceTexts[0].innerText = q.A;
+      choiceTexts[1].innerText = q.B;
+      choiceTexts[2].innerText = q.C;
+      choiceTexts[3].innerText = q.D;
+    }
 
-        createQuestion();
-    })
-
-    .catch(error => console.error('Error loading JSON:', error));
-
+    showRandomQuestion(); // Call when page loads
+  })
+  .catch(error => console.error("Error loading questions:", error));
