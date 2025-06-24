200a97241897efbb1a7e32f20dcaa371df5c6d42
Juno Joseph <87766026+Juno-Joseph@users.noreply.github.com>
2025-06-23 20:11:23 -0400

    implemented the server side js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
+app.use(express.static(path.join(__dirname, 'public')))
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use((req, res) => {
+    res.status(404);
+    res.send(`<h1>Error 404: Resource Not Found</h1>`)
 })
 
 app.listen(port, () => {
